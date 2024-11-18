db29f4b037814ab427ce80005dfe6303c51eb64b
Ghlijyan Hakob <hakobghlijyan@gmail.com>
2024-11-18 15:36:30 +0400

    Initial Commit

---
+++ SwiftUIBottomSheetAnimationAppleMusic/SwiftUIBottomSheetAnimationAppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBottomSheetAnimationAppleMusicApp.swift
+//  SwiftUIBottomSheetAnimationAppleMusic
+//
+//  Created by Hakob Ghlijyan on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBottomSheetAnimationAppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
