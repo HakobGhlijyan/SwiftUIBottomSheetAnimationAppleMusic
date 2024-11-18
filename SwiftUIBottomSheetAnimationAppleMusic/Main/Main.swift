1447eda730e9392c5cc0382fabe3dd9e0bd233dc
Ghlijyan Hakob <hakobghlijyan@gmail.com>
2024-11-18 15:39:31 +0400

    [Start] add temp

---
+++ SwiftUIBottomSheetAnimationAppleMusic/Main/Main.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  SwiftUIBottomSheetAnimationAppleMusic
+//
+//  Created by Hakob Ghlijyan on 18.11.2024.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Home()
+    }
+}
+
+#Preview {
+    Main()
+}
---
+++ SwiftUIBottomSheetAnimationAppleMusic/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  SwiftUIBottomSheetAnimationAppleMusic
+//
+//  Created by Hakob Ghlijyan on 18.11.2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    Main()
+}
